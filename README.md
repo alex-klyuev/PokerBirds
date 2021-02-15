ed786c143bc9e3631da16a054128d6ddc6f8a5d1
Alex Klyuev <aklyuev96@gmail.com>
2021-02-15 14:19:50 -0800

    add cards library

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PokerBirds
 Poker Game created in React, Node, Mongo, and Express
+
+Card Graphics created by cardsJS:
+https://github.com/richardschneider/cardsJS
+http://richardschneider.github.io/cardsJS/
--- client/src/components/table/Board.jsx
+++ client/src/components/table/Board.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Board = (props) => {
-
-  return (
+const Board = (props) => (
+  <div>
     <div>Board</div>
-  );
-}
+    <img alt="" className="card" src="lib/cards/Blue_Back.svg" />
+  </div>
+);
 
 Board.propTypes = {
 
