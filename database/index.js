40b4b23df553ca523131bf256b2a7f7908bdea1e
Alex Klyuev <aklyuev96@gmail.com>
2021-03-25 18:42:44 -0700

    update index and bundle files to avoid bundle.js issue

--- database/index.js
+++ database/index.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 // for local use:
 /* mongoose.connect('mongodb://localhost/PokerBirds', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
-  console.log('Connected to mongo');
+  console.log('Connected to Mongo');
 }); */
 
 // for deployment through docker:
 mongoose.connect('mongodb://database:27017/PokerBirds', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
-  console.log('Connected to mongo');
+  console.log('Connected to Mongo');
 });
